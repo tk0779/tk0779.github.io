28e24102a0dfb970d36891b2a4de8e2ac7c5f3b7
tk0779 <tirnat@gmail.com>
2023-02-25 14:40:12 +0700

    Updates

---
+++ precache-manifest.02b50132add5a4df24991034d15f8ae1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07f774352744cfe1b3f9",
+    "url": "/react-gh-pages/static/css/main.c43316d2.chunk.css"
+  },
+  {
+    "revision": "07f774352744cfe1b3f9",
+    "url": "/react-gh-pages/static/js/main.2cde9ecd.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "d0cd6d99a2bdee8d5e14",
+    "url": "/react-gh-pages/static/js/2.805c071e.chunk.js"
+  },
+  {
+    "revision": "dd01821644931717e349745918866361",
+    "url": "/react-gh-pages/index.html"
+  }
+];
